25c432404c6b69b247bcb9769456e9be63b5bbb1
Leonardo Puebla <leonardo38361@gmail.com>
2021-11-29 23:19:23 -0300

    limpio el código, borro código de seteo

--- figuras.js
+++ figuras.js
@@ -2,12 +2,6 @@
  * Código del cuadrado
  */
 console.group("Cuadrados");
-// const ladoCuadrado = 5;
-
-// console.log("Los lados del cuadrado miden: "
-//     + ladoCuadrado
-//     + "cm"
-// );
 
 function perimetroCuadrado(lado) {
     return lado * 4;
@@ -15,72 +9,31 @@ function perimetroCuadrado(lado) {
 
 perimetroCuadrado();
 
-// console.log("El perimetro del cuadrado es: "
-//     + perimetroCuadrado
-//     +
-//     "cm"
-// );
-
 function areaCuadrado(lado) {
     return lado * lado;
 }
-// console.log("El área del cuadrado es: "
-//     + areaCuadrado
-//     + "cm^2"
-// );
+
 console.groupEnd();
 
 /**
  * Código del triángulo
  */
 console.group("Triángulos")
-// const ladoTriangulo1 = 6;
-// const ladoTriangulo2 = 6;
-// const baseTriangulo = 4;
-
-// console.log("Los lados del triángulo miden: "
-//     + ladoTriangulo1
-//     + "cm, "
-//     +
-//     ladoTriangulo2
-//     + "cm, "
-//     + baseTriangulo
-//     + "cm"
-// );
-
-// const alturaTriangulo = 5.5;
-// console.log("La altura del triángulo es de: "
-//     + alturaTriangulo
-//     + "cm"
-// );
 
 function perimetroTriangulo(lado1, lado2, base) {
     return lado1 + lado2 + base;
 }
-// console.log("El perimetro del Triángulo es: "
-//     + perimetroTriangulo
-//     +
-//     "cm"
-// );
 
 function areaTriangulo(base, altura) {
     return (base * altura) / 2;
 }
 
-// console.log("El área del triángulo es: "
-//     + areaTriangulo
-//     + "cm^2"
-// );
 console.groupEnd();
 
 /**
  * Círculos
  */
 console.group("Círculos");
-
-//  Radio
-// const radioCirculo = 4;
-// console.log("El radio del círculo es: " + radioCirculo + "cm")
 
 //  Diámetro
 function diametroCirculo(radio) {
@@ -103,6 +56,4 @@ function areaCirculo(radio) {
     return (radio * radio) * PI;
 }
 
-// const areaCirculo = (radioCirculo * radioCirculo) * PI;
-// console.log("El área del círculo es: " + areaCirculo + "cm^2");
 console.groupEnd();

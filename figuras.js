bb76011b7904002e4b053e522d5eba843ee59bd2
Leonardo Puebla <leonardo38361@gmail.com>
2021-11-30 02:19:23 -0300

    Interaccion entre html en js y se agrega css

--- figuras.js
+++ figuras.js
@@ -57,3 +57,18 @@ function areaCirculo(radio) {
 }
 
 console.groupEnd();
+
+//Aquí interactúo con HTML
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(area);
+}

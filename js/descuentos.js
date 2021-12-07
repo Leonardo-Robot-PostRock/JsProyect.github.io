4827a281b78adf7cbcfddc9d9bce999c63f9e33b
Leonardo Puebla <leonardo38361@gmail.com>
2021-12-07 05:06:38 -0300

    Se agregaron estilos p/ figuras.html y se crea el diseñor p/mobile de descuentos.html

--- js/descuentos.js
+++ js/descuentos.js
@@ -1,12 +1,27 @@
-const precioOriginal = 100;
-const descuento = 15;
+// const precioOriginal = 120;
+// const descuento = 18;
 
-const porcentajePrecioConDescuento = 100 - descuento;
-const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100;
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento
+// });
 
-console.log({
-    precioOriginal,
-    descuento,
-    porcentajePrecioConDescuento,
-    precioConDescuento
-});
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+
+}

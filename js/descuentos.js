414d2145a14ffa46fc61e6859c95072a7d86acbe
Leonardo Puebla <leonardo38361@gmail.com>
2021-12-09 02:10:02 -0300

    exhaustivo cambios en css, se implementa una función para encontrar valores del array coupons(...), se modifica el html en inputDiscount p/ inputCoupon

--- js/descuentos.js
+++ js/descuentos.js
@@ -1,27 +1,98 @@
-// const precioOriginal = 120;
-// const descuento = 18;
-
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
 }
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento
-// });
 
 function onClickButtonPriceDiscount() {
+
+    // const coupons = [
+    //     "argen10",
+    //     "argen15",
+    //     "argen20",
+    //     "argen25"
+    // ]
+
+    /** 
+         Otra posible solución es con switch
+         */
+
+    /**
+     *   switch(couponValue) {
+          case coupons[0]: // "JuanDC_es_Batman"
+            descuento = 15;
+          break;
+          case coupons[1]: // "pero_no_le_digas_a_nadie"
+            descuento = 30;
+          break;
+          case coupons[2]: // "es_un_secreto"
+           descuento = 25;
+         break;
+        }
+     */
+
+    /**
+     * Esta es una solución con if, sin embargo un alerta no es lo más cómodo para el usuario en el caso de que se ingrese un cupón no válido.
+     */
+
+    /** 
+    *  if (!coupons.includes(couponValue)) {
+        alert("El cupón " + couponValue + " no es válido");
+    } else if (couponValue === coupons[0]) {
+        descuento = 10;
+    } else if (couponValue === coupons[1]) {
+        descuento = 15;
+    } else if (couponValue === coupons[2]) {
+        descuento = 20;
+    } else if (couponValue === coupons[3]) {
+        descuento = 25;
+    }
+    ----------------------------------------------------------------------*/
+
+    let descuento;
     const inputPrice = document.getElementById("inputPrice");
     const priceValue = inputPrice.value;
-    const inputDiscount = document.getElementById("inputDiscount");
-    const discountValue = inputDiscount.value;
+    const inputCoupon = document.getElementById("inputCoupon");
+    const couponValue = inputCoupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    /**
+     * Preparo el array para usarlo en una función más inteligente ------------------------>
+     */
+    const coupons = [
+        {
+            name: "argen10",
+            descuento: 10,
+        },
+        {
+            name: "argen15",
+            descuento: 15,
+        },
+        {
+            name: "argen20",
+            descuento: 20,
+        },
+        {
+            name: "argen25",
+            descuento: 25,
+        }
+    ]
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
 
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+    const userCoupon = coupons.find(isCouponValueValid);
+    
 
+    if (!userCoupon) {
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El cupón: " + couponValue + " no es válido";
+    } else {
+        const descuento = userCoupon.descuento;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+        const resultP = document.getElementById("ResultP");
+
+        resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+    }
 }
+

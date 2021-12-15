f421bc8dd6690222297b63dcb0e508887b486370
Leonardo Puebla <leonardo38361@gmail.com>
2021-12-15 20:45:57 -0300

    Analisis del problema, calculo de mediana en tanto si nuestra lista es par o impar; según eso se ejecuta una condición

--- js/mediana.js
+++ js/mediana.js
@@ -6,3 +6,20 @@ const lista1 = [
 ]
 
 const mitadLista1 = lista1.length / 2;
+
+function esPar(numero) {
+    if (numero es par ?) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+if (lista1 es par ?) {
+    // necesitamos dos elementos
+    // -> el promedio
+    // -> mediana
+}else {
+    // posición mitadLista1 dentro Lista1
+    // -> mediana
+}
